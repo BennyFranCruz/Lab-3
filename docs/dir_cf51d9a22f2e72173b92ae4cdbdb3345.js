12e01ba3a278c5934964b72deb825922148cdd14
BennyFranCruz <123684992+BennyFranCruz@users.noreply.github.com>
2023-02-16 10:24:49 -0800

    Doxygen

---
+++ docs/dir_cf51d9a22f2e72173b92ae4cdbdb3345.js
@@ -0,0 +1,11 @@
+var dir_cf51d9a22f2e72173b92ae4cdbdb3345 =
+[
+    [ "basic_tasks.py", "basic__tasks_8py.html", "basic__tasks_8py" ],
+    [ "cotask.py", "cotask_8py.html", "cotask_8py" ],
+    [ "encoder_reader.py", "encoder__reader_8py.html", "encoder__reader_8py" ],
+    [ "main.py", "main_8py.html", "main_8py" ],
+    [ "motor_driver.py", "motor__driver_8py.html", "motor__driver_8py" ],
+    [ "porportional_controller.py", "porportional__controller_8py.html", "porportional__controller_8py" ],
+    [ "Serial_Reader.py", "_serial___reader_8py.html", "_serial___reader_8py" ],
+    [ "task_share.py", "task__share_8py.html", "task__share_8py" ]
+];
